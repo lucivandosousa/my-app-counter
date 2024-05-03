910e49f62a40c7c0b40e47d96b55377cd19b77b7
Lucivando Sousa <lucivandosousa@gmail.com>
2024-05-03 10:28:00 -0300

    feat: add project

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, StyleSheet, Button, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [count, setCount] = useState(0)
+
+  function adicionar() {
+    setCount(count + 1)
+  }
+
+  function remover() {
+    if (count === 0) {
+      Alert.alert("Count", "O valor está zerado")
+      return
+    }
+    setCount(count - 1)
+  }
+
+  function limpar() {
+    setCount(0)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={styles.title}>App Contador</Text>
+      <Text style={styles.countText}>Count: {count}</Text>
+      <View style={styles.button}>
+        <Button title='Adicionar (+)' onPress={adicionar} />
+      </View>
+      <View style={styles.button}>
+        <Button style={styles.button} title='Remover (-)' onPress={remover} />
+      </View>
+      <View style={styles.button}>
+        <Button style={styles.button} title='Limpar' onPress={limpar} />
+      </View>
     </View>
   );
 }
@@ -13,8 +43,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#B0C4DE',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  countText: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  button: {
+    minWidth: 200,
+    marginBottom: 10,
+  }
 });
---
+++ readme.md
@@ -0,0 +1,5 @@
+# My App Counter
+
+Este é meu primeiro aplicativo criado com React Native. Fugindo do Hello World.
+
+O aplicativo é simples e utiliza recursos comuns em aplicações React, como hooks e interação com componentes por meio de eventos.
